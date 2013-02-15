f6b8f899b74fcc50dd963ac40faa5c61b9ebd1de
Mitch <mitchbrannam@gmail.com>
2013-02-14 20:24:23 -0800

    Random error in Downtown fix

--- CyberCity/src/citygenerator/generator/Downtown.java
+++ CyberCity/src/citygenerator/generator/Downtown.java
@@ -158,8 +158,8 @@ public class Downtown extends Graph {
 			}
 			else{
 				x=root-1;
-				if(currentZ > 0)
-					z = new Random().nextInt(currentZ);
+				if(currentZ > 1)
+					z = new Random().nextInt(currentZ - 1);
 				else
 					z = new Random().nextInt(root-1);
 			}
