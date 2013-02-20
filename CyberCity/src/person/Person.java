2fee2c4dd666b3bf91618e8230b9940099d384f7
Joubin Jabbari <joubin.j@gmail.com>
2013-02-19 20:23:35 -0800

    Adding person tasks.

--- CyberCity/src/person/Person.java
+++ CyberCity/src/person/Person.java
@@ -1,14 +1,21 @@
 package person;
 
+//All attributes that makes a "person" will exist here
+
 import java.util.Date;
+import java.util.PriorityQueue;
 
 public class Person {
-	public enum genders{
+	public enum genders {
 		MALE, FEMALE;
 	}
+
 	public void person() {
 		String name;
 		Date birthDate;
 		genders gender;
 	}
+	
+	PriorityQueue<Task> personPriorityQueue = new PriorityQueue<Task>();
+	
 }
